a302d8a580875ceb6a4f7d640965da66057f8606
wxodi0 <teayang06@gmail.com>
2023-12-27 23:40:26 +0900

    feat: NoArgsConstructor를 이용한 Comment 생성자 추가

--- src/main/java/com/example/anonymousboard/domain/Comment.java
+++ src/main/java/com/example/anonymousboard/domain/Comment.java
@@ -1,14 +1,13 @@
 package com.example.anonymousboard.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +19,16 @@ public class Comment {
     private String writer;
     private String title;
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id")
+    @JsonIgnore
+    private Content contents;
+
+    public Comment(String pw, String writer, String title, String content) {
+        this.pw = pw;
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
 }
