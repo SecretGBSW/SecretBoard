c06c1f45abaa1e49701f9596964a4953878d4c56
wxodi0 <teayang06@gmail.com>
2023-12-28 14:03:28 +0900

    feat: Security를 통한 http 요청 허용

---
+++ src/main/java/com/example/anonymousboard/auth/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.anonymousboard.auth.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable
+                        )
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
