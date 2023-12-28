cc5165049696165429ce81d4997fe7911d021bb8
wxodi0 <teayang06@gmail.com>
2023-12-29 00:08:11 +0900

    feat: csrf, cors, authorizeHttpRequests 권한 추가 및 요청에 따른 모든 권한 허용

--- src/main/java/com/example/anonymousboard/auth/config/SecurityConfig.java
+++ src/main/java/com/example/anonymousboard/auth/config/SecurityConfig.java
@@ -1,29 +1,43 @@
 package com.example.anonymousboard.auth.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .headers((headerConfig) ->
-                        headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable
-                        )
-                )
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers("/**").permitAll()
-                                .anyRequest().authenticated()
-                );
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(AbstractHttpConfigurer::disable)
+      .headers((headerConfig) ->
+        headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable
+        )
+      )
+      .authorizeHttpRequests((authorize) ->
+        authorize
+
+          .requestMatchers(
+            "/**"
+          ).permitAll()
+
+          .requestMatchers(HttpMethod.GET).permitAll()
+          .requestMatchers(HttpMethod.POST).permitAll()
+          .requestMatchers(HttpMethod.PUT).permitAll()
+          .requestMatchers(HttpMethod.DELETE).permitAll()
+
+          .anyRequest().authenticated()
+      );
+    return http.getOrBuild();
+  }
 }
