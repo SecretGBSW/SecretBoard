65e364177637d5af03e24dd8a4c2b9ba142e05b4
wxodi0 <teayang06@gmail.com>
2023-12-28 10:10:12 +0900

    feat: 카테고리 controller 추가

--- src/main/java/com/example/anonymousboard/category/controller/CategoryController.java
+++ src/main/java/com/example/anonymousboard/category/controller/CategoryController.java
@@ -1,7 +1,7 @@
-package com.example.anonymousboard.controller;
+package com.example.anonymousboard.category.controller;
 
-import com.example.anonymousboard.domain.Category;
-import com.example.anonymousboard.service.CategoryService;
+import com.example.anonymousboard.category.domain.Category;
+import com.example.anonymousboard.category.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
