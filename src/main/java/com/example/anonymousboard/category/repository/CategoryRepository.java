ed694bbeb88ab3d8ae321ed3addb0578bc64fcac
wxodi0 <teayang06@gmail.com>
2023-12-28 10:09:49 +0900

    feat: 카테고리 레포 추가

--- src/main/java/com/example/anonymousboard/category/repository/CategoryRepository.java
+++ src/main/java/com/example/anonymousboard/category/repository/CategoryRepository.java
@@ -1,5 +1,5 @@
-package com.example.anonymousboard.repository;
-import com.example.anonymousboard.domain.Category;
+package com.example.anonymousboard.category.repository;
+import com.example.anonymousboard.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
