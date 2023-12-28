c6d088b115670d21fe21de3c59c8a3fb1243ff13
wxodi0 <teayang06@gmail.com>
2023-12-29 02:56:41 +0900

    feat: responseListDataDto 수정

--- src/main/java/com/example/anonymousboard/board/dto/response/ResponseListDataDto.java
+++ src/main/java/com/example/anonymousboard/board/dto/response/ResponseListDataDto.java
@@ -1,12 +1,8 @@
 package com.example.anonymousboard.board.dto.response;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
